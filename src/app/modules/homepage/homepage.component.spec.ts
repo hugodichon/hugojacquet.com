bd60de45f3b8daa15e9ce27c61bfc5a5a907a613
Hugo Jacquet <jacquet.hugo@gmail.com>
2019-05-22 21:58:28 +0200

    Add the test draft for the homepage

---
+++ src/app/modules/homepage/homepage.component.spec.ts
@@ -0,0 +1,13 @@
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('HomepageComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+            ],
+            declarations: []
+        });
+    }));
+});
